3549162be004972d17e557d34a5d38abd4191433
ximus <corneliussamuel@gmail.com>
2026-03-30 06:33:01 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-// import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext";
 import AuthProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
