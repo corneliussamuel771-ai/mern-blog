372121c93c48299d27535da4c1eac9ce48844972
ximus <corneliussamuel@gmail.com>
2026-03-30 05:41:56 -0700

    Fix import paths for ThemeContext and AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-import AuthProvider from "./Context/AuthContext.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
